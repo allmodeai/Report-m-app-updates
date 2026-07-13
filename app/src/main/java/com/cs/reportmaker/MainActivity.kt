f30945021a346c6279c0dc0c2d4284d854cae602
allmode <allmodeai@gmail.com>
2026-07-14 00:22:45 +0530

    Updated app version to 1.4.1, Fixed bug: Added WebChromeClient for Load button functionality, PDF Save and Print buttons not working and bumped versionCode to 6

--- app/src/main/java/com/cs/reportmaker/MainActivity.kt
+++ app/src/main/java/com/cs/reportmaker/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.cs.reportmaker
+import android.webkit.WebChromeClient
+import android.net.Uri
+import android.webkit.ValueCallback
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
+import android.webkit.JavascriptInterface
+import android.widget.Toast
+import android.print.PrintAttributes
+import android.print.PrintManager
+import android.content.Context
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var myWebView: WebView
+    lateinit var myWebView: WebView
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +35,16 @@ class MainActivity : AppCompatActivity() {
             javaScriptEnabled = true
             domStorageEnabled = true
         }
+        // THIS IS THE MISSING PIECE FOR YOUR "LOAD" BUTTON
+        myWebView.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(
+                webView: WebView,
+                filePathCallback: ValueCallback<Array<Uri>>,
+                fileChooserParams: FileChooserParams
+            ): Boolean {
+                return super.onShowFileChooser(webView, filePathCallback, fileChooserParams)
+            }
+        }
 
         // Connect the Bridge to the HTML
         // This links the Java class we made to the "AndroidBridge" name in your HTML
@@ -35,3 +55,34 @@ class MainActivity : AppCompatActivity() {
         myWebView.loadUrl("file:///android_asset/index.html")
     }
 }
+// This is the "Bridge" class that talks to your HTML
+class AndroidBridge(private val context: Context) {
+
+    @JavascriptInterface
+    fun saveBackup(data: String, fileName: String) {
+        try {
+            // Defines the folder: Android/data/com.cs.reportmaker/files/Documents/
+            val file = File(context.getExternalFilesDir(android.os.Environment.DIRECTORY_DOCUMENTS), fileName)
+            FileOutputStream(file).use { output ->
+                output.write(data.toByteArray())
+            }
+            // Show a small popup to confirm the save
+            Toast.makeText(context, "Saved to: " + file.absolutePath, Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            Toast.makeText(context, "Error saving file: " + e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @JavascriptInterface
+    fun printPDF() {
+        val activity = context as MainActivity
+        activity.runOnUiThread {
+            // We use the activity's myWebView property directly
+            val webView = activity.myWebView
+
+            val printManager = activity.getSystemService(Context.PRINT_SERVICE) as PrintManager
+            val printAdapter = webView.createPrintDocumentAdapter("Report_Job")
+
+            printManager.print("Report_Document", printAdapter, PrintAttributes.Builder().build())
+        }
+    }
